07105f962ce3300618003c132216d1d2587d834f
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-02-21 23:32:00 +0100

    added new images and updated where.tsx
    adds four new images to the src/assets/images folder and updates the `where.tsx` file to add the new images.
    
    the images that were added are `bd-logo.png`, `magalu.jpg`, `anchor.jpg`, and `magalupay.jpg`. in the `where.tsx` file, these images were added as references in the co component, which was used to display information about various companies. the references were also used to update the role, tagline, and what of each company.

--- src/pages/Where.tsx
+++ src/pages/Where.tsx
@@ -1,8 +1,10 @@
 import PageWrapper from './PageWrapper';
 import Co from '../components/Co';
 import styled from 'styled-components';
-import HopIcon from '../assets/images/hop-social-icon.png';
-import HivenAppIcon from '../assets/images/hiven-app-icon.png';
+import DefenceLabsIcon from '../assets/images/bd-logo.png';
+import MagaluIcon from '../assets/images/magalu.jpg';
+import AnchorLoansIcon from '../assets/images/anchor.jpg';
+import MagaluPay from '../assets/images/magalupay.jpg';
 import GigglAppIcon from '../assets/images/giggl-app-icon.jpg';
 import HonkAppIcon from '../assets/images/honk-app-icon.jpg';
 import Repo from '../components/Repo';
@@ -20,48 +22,47 @@ const Where = () => {
         <Co
           url="https://hop.io"
           name="Defence Labs"
-          iconReference={HopIcon}
+          iconReference={DefenceLabsIcon}
           tagline="Private Data"
           role={'Senior Software Engineer & Core Developer'}
-          what={
-            `I was tired of the friction and overhead that came with backend deployment. Hop streamlines the deployment experience, with delight included. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-            `
-          }
+          pretext={'Currently'}
+          what={`
+ Rebuilding the IT department from scratch drived by creating fast and innovative projects  using the most up-to-date technologies in a serverless environment, releasing new products and integrations, making them also accessible through developer tools.`}
         />
         <Co
-          url="https://hiven.io"
+          url="https://www.magazineluiza.com.br"
           name="Magazine Luiza"
-          iconReference={HivenAppIcon}
+          iconReference={MagaluIcon}
           tagline="Retail Company"
           role={'Senior Software Engineer & Core Developer'}
+          pretext={'2022-2018'}
           what={`- Responsibilities include product development from the software development perspective, addressing performance or security concerns, as well as acting as architect.
 - High scalable, high impact products through the use of technology, team work and best software engineering practices in high collaborative and challenging environments.
 - At times my role included helping devops or using my previous data-engineering experience to help with developing analytics systems. Though the title suggests more programming-oriented work, much of my time is spent with architectural changes, or advising other developers or staff on possible ways to proceed toward a goal.`}
-          acquired
         />
         <Co
-          url="https://giggl.app"
-          name="Anchor Loans"
-          iconReference={GigglAppIcon}
+          role={'Senior Software Engineer'}
+          url="https://fintechmagalu.com.br"
+          name="MagaluPay"
+          iconReference={MagaluPay}
           tagline="Financial Services"
-          role={'Senior Software Engineer & Core Developer'}
-          pretext={'2020-2022'}
+          pretext={'2021'}
           what={
-            `- Development of systems controlling the buying and selling processes of properties owned by the company. 
-- Developed a subsystem for neighbor property comparison.
-- Developed web scrapers and internal ETL tools.
-- Developed a system for property construction process management.
-- Experimented with different data structures to maintain a large database efficiently.`
+            '- Development of the core features that a digital bank would have to be succeed such as payment ransactions, payments by QRcode, external links, wallet account services and even cashback rules. Also responsible to create Integrations with third-parties partners and internal-company apps.'
           }
         />
         <Co
-          role={'Backend Engineer'}
-          url="https://honk.me"
-          name="Honk"
-          iconReference={HonkAppIcon}
-          tagline="Consumer social"
-          pretext={'2020'}
-          what={'- data mining, big data, machine learning, high availability web rest api, web scraping and more.'}
+          url="https://www.anchorloans.com"
+          name="Anchor Loans"
+          iconReference={AnchorLoansIcon}
+          tagline="Financial Services"
+          role={'Senior Software Engineer & Core Developer'}
+          pretext={'2018-2015'}
+          what={`- Development of systems controlling the buying and selling processes of properties owned by the company. 
+- Developed a subsystem for neighbor property comparison.
+- Developed web scrapers and internal ETL tools.
+- Developed a system for property construction process management.
+- Experimented with different data structures to maintain a large database efficiently.`}
         />
       </CoWrapper>
 

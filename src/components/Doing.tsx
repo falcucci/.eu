23bab80fdbc07caa828acb1c2f59cddf4570274b
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-02-21 17:53:44 +0100

    adding the apple logo instead
    changes:
    
    - src/assets/images/apple-logo.svg
    - src/assets/images/vim-logo.svg
    - src/components/Doing.tsx

--- src/components/Doing.tsx
+++ src/components/Doing.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styled, { keyframes } from 'styled-components';
 import { Presence } from '../types/lanyard';
 import SpotifyLogo from '../assets/images/spotify-logo.svg';
-import VimLogo from '../assets/images/vim-logo.svg';
+import AppleLogo from '../assets/images/apple-logo.svg';
 import { useAtom } from 'jotai';
 import { doingAtom } from '../state/lanyard';
 
@@ -121,7 +121,7 @@ const Doing = (
                 <ActivityImage
                   src={`https://cdn.discordapp.com/app-assets/383226320970055681/565945077491433494.png`}
                 />
-                <ActivitySecondaryImage src={VimLogo} />
+                <ActivitySecondaryImage src={AppleLogo} />
               </ActivityImageContainer>
             ) }
             <ActivityInfo>

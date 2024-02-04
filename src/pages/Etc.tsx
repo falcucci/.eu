7dc2083d3d6aebe658f42c34b8883467f5b7adf2
Alexsander Falcucci <alex.falcucci@gmail.com>
2024-02-04 19:46:42 +0100

    chore: update contact information and add new cryptocurrency address
    - add a link to reach out through telegram in the contact section
    - update the email contact in the contact section
    - add a new cryptocurrency address in the other section

--- src/pages/Etc.tsx
+++ src/pages/Etc.tsx
@@ -10,18 +10,25 @@ const Etc = () => (
     <h1>/etc</h1>
     <h2>Contact</h2>
     <p>
-      I'm most responsive through Telegram, you can{' '}
-      <a href={'https://t.me/falcucci'}>click here</a> to reach me out.
+      I'm most responsive through Telegram, you can <a href={'https://t.me/falcucci'}>click here</a>{' '}
+      to reach me out.
     </p>
     <p>
-      If your inquiry is related to a tool I manage or control, please
-      send an email to my email listed on the respective WHOIS contact. If it's an emergency, you
-      can email <a href="mailto:alex.falcucci@gmail.com">alex.falcucci@gmail.com</a>.
+      If your inquiry is related to a tool I manage or control, please send an email to my email
+      listed on the respective WHOIS contact. If it's an emergency, you can email{' '}
+      <a href="mailto:alex.falcucci@gmail.com">alex.falcucci@gmail.com</a>.
     </p>
     <h2>Other</h2>
     <ul>
       <li>
         BTC: <code>14D2Qark9CPgb4sY7rK7qY6nKYGe3jPydR</code>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <code>
+          addr1qxthajcwcs4sxd29wtpvhfv3hxmyptqu38scetmxcuwtnkanyel5dahkpf7vf4t7mu8euwwn9h00n7jgv0cqdh62mxss04sja2
+        </code>
       </li>
     </ul>
   </PageWrapper>
